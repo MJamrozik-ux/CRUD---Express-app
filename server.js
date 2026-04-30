bc7e09c5c92f5df06e996867121fb0a210f7a800
MJamrozik-ux <mikolajjamrozik81@gmail.com>
2026-04-30 11:37:48 +0200

    Dodanie endpointa /users

--- server.js
+++ server.js
@@ -1,21 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.url, {
-}).then(() => {
-    console.log("Databse Connected Successfully!!");    
-}).catch(err => {
-    console.log('Could not connect to the database', err);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const dbConfig = require("./config/database.config.js");
+app.listen(3000);
+mongoose.connect(dbConfig.url)
+  .then(function () {
+    console.log("Database Connected Successfully!!");
+  })
+  .catch(function (err) {
+    console.log("Could not connect to the database", err);
     process.exit();
-});
-app.get('/', (req, res) => {
-    res.json({"message": "Hello Crud Node Express"});
-});
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  });
+
+const User = require("./models/Users.js");
+//CREATE
+app.post("/users", async function (req, res) {
+  try {
+    const name = req.body.name;
+    const age = req.body.age;
+
+    const user = new User({
+      name: name,
+      age: age
+    });
+
+    const savedUser = await user.save();
+
+    res.send(savedUser);
+
+  } catch (err) {
+    res.status(500).send({
+      message: "Error creating user"
+    });
+  }
 });
